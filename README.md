a47c1df4c0ad19a020410616023db08b02d754ee
IsDaDev <115308963+IsDaDev@users.noreply.github.com>
2025-10-27 15:52:48 +0100

    added a seperate config file

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ A Python + C++ tool to automatically dump, parse, and archive Windows EVTX event
 windows-eventlog-archiver/
 │
 ├── backupLogs.py               # Main script – exports, parses, validates, and archives EVTX logs
+├── config.py                # Configuration file
 ├── validator.py                # Validates JSON output for correctness
 ├── readEvtx.cpp                # Reads Windows Event Logs (.evtx) and dumps as plaintext
 ├── evtx2json.cpp               # Converts raw EVTX data to structured JSON
@@ -40,6 +41,43 @@ windows-eventlog-archiver/
    - If the size is greater than 100mb, the files are zipped
 
 ## How to install and run
+
+### Configuration-file
+
+`sample`
+
+```python
+from os import path
+import os
+import socket
+
+# Unique Identifier if multiple logs are safed at the same destination
+uuid = os.getlogin() + "_" + socket.gethostname()
+
+# array containing all logs to collect
+logTypes = ["Security", "Application", "System"]
+
+# max filesize before they get zipped in mb
+chunkSize = 100 * 1024 * 1024 # 100mb
+
+
+# ======================================================================
+# paths are based on the file location of the executable file by default
+# ======================================================================
+
+# base path, default where the executable lies
+basePath = path.abspath(path.dirname(__file__))
+
+# location where the logs are safed
+logDirectory = path.join(basePath, "logs", uuid)
+
+# location of raw logs and the parsed logs, default location is inside the logdirectory and UUID
+rawDirectory = path.join(logDirectory, "raw")
+parsedDirectory = path.join(logDirectory, "parsed")
+
+# location for the finished zip files
+zipDirectory = path.join(basePath, "zips")
+```
 
 ### Prerequesites
 
--- backupLogs.py
+++ backupLogs.py
@@ -7,38 +7,33 @@ import sys
 import socket
 import zipfile
 
+import config
+
 uuid = os.getlogin() + "_" + socket.gethostname()
 basePath = os.path.dirname(os.path.realpath(__file__))
 
 pjoin = os.path.join
 
-logDirectory = pjoin(basePath, "logs", uuid)
-rawDirectory = pjoin(logDirectory, "raw")
-parsedDirectory = pjoin(logDirectory, "parsed")
-zipDirectory = pjoin(basePath, "zips")
-
-alerts = ["Security", "Application", "System"]
-
 ts = time.strftime("%Y-%m-%d_%H-%M-%S")
 
-os.makedirs(logDirectory, exist_ok=True)
-os.makedirs(rawDirectory, exist_ok=True)
-os.makedirs(parsedDirectory, exist_ok=True)
-os.makedirs(zipDirectory, exist_ok=True)
+os.makedirs(config.logDirectory, exist_ok=True)
+os.makedirs(config.rawDirectory, exist_ok=True)
+os.makedirs(config.parsedDirectory, exist_ok=True)
+os.makedirs(config.zipDirectory, exist_ok=True)
 
 def clearDirectory():
-    files = os.listdir(rawDirectory)
+    files = os.listdir(config.rawDirectory)
 
     for file in files:
-        p = pjoin(rawDirectory, file)
+        p = pjoin(config.rawDirectory, file)
         os.remove(p)
 
 def createZip(files, timestamp):
     print("Creating zip file ...")
-    zipPath = f"{zipDirectory}/{timestamp}_{uuid}.zip"
+    zipPath = f"{config.zipDirectory}/{timestamp}_{config.uuid}.zip"
     with zipfile.ZipFile(zipPath, "w", compression=zipfile.ZIP_DEFLATED) as zipf:
         for file in files:
-            abs_path = pjoin(parsedDirectory, file)
+            abs_path = pjoin(config.parsedDirectory, file)
             zipf.write(abs_path, arcname=file)
             os.remove(abs_path)
     print("Zip file created!")
@@ -57,16 +52,14 @@ def executeEVTX(alert, outFile, parsed):
     subprocess.run(verify, check=True)
 
 def startUploadIfOK(timestamp):
-    BLOBSIZE = 100 * 1024 * 1024 # 100mb
-
-    files = os.listdir(parsedDirectory)
+    files = os.listdir(config.parsedDirectory)
     size = 0
     for file in files:
-        size += os.path.getsize(pjoin(parsedDirectory, file))
+        size += os.path.getsize(pjoin(config.parsedDirectory, file))
     
     clearDirectory()
 
-    if size >= BLOBSIZE:
+    if size >= config.chunkSize:
         print("Uploading ...") 
         createZip(files, timestamp)
     else:
@@ -84,9 +77,9 @@ def main():
         sys.exit()
 
     threads = []
-    for alert in alerts:
-        rawFilePath = pjoin(rawDirectory, f"{ts}_{alert}").replace("\\", "/")
-        parsedFilePath = pjoin(parsedDirectory, f"{ts}_{alert}.json").replace("\\", "/")
+    for alert in config.logTypes:
+        rawFilePath = pjoin(config.rawDirectory, f"{ts}_{alert}").replace("\\", "/")
+        parsedFilePath = pjoin(config.parsedDirectory, f"{ts}_{alert}.json").replace("\\", "/")
         
         t = threading.Thread(target=executeEVTX, args=(alert, rawFilePath, parsedFilePath))
         t.start()
